a980deeb4079762b9de48e1db602071b06def246
baby7305 <songhaibei7305@163.com>
2020-01-14 11:00:54 +0800

    变量规则
    http://localhost:5000/user/baby
    http://localhost:5000/post/4

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, escape
 
 app = Flask(__name__)
 
@@ -11,6 +11,24 @@ def hello_world():
 @app.route('/index')
 def index():
     return 'Index Page'
+
+
+@app.route('/user/<username>')
+def show_user_profile(username):
+    # show the user profile for that user
+    return 'User %s' % escape(username)
+
+
+@app.route('/post/<int:post_id>')
+def show_post(post_id):
+    # show the post with the given id, the id is an integer
+    return 'Post %d' % post_id
+
+
+@app.route('/path/<path:subpath>')
+def show_subpath(subpath):
+    # show the subpath after /path/
+    return 'Subpath %s' % escape(subpath)
 
 
 if __name__ == '__main__':
