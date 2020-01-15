c1d840ccb40c9d9c3ce86babe60a4e667e4ddccd
baby7305 <songhaibei7305@163.com>
2020-01-15 16:34:41 +0800

    调整路由

--- app.py
+++ app.py
@@ -19,7 +19,7 @@ def allowed_file(filename):
            filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
 
 
-@app.route('/', methods=['GET', 'POST'])
+@app.route('/upload', methods=['GET', 'POST'])
 def upload_file():
     if request.method == 'POST':
         # check if the post request has the file part
